406ed8398daa730ce27ee05aa881c9825a6bed96
Alex Griessman <alex.griessman@gmail.com>
2012-08-14 19:35:22 -0400

    Phase 4 of the transient solver implementation.
    This is nearly the final step! The code compiles cleanly and solves
    the heat equation in one dimension.
    
    Stuff still left to do:
     * Make a nifty function to simplifiy the transient solver.
     * Verify the solution's accuracy.
     * Add in functions to output stuff nicely.
     * Fix all the horrible memory leaks that are likely lurking about.

---
+++ matrix.h
@@ -0,0 +1,59 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+/* Macro to shorten code where a number is added to the current value of an
+ * element in a matrix. */
+#define addval(A, VAL, I, J) setval((A), (VAL) + val((A), (I), (J)), (I), (J))
+
+typedef struct {
+    double **array;
+    int rows;
+    int cols;
+} matrix;
+
+void DestroyMatrix(matrix*);
+matrix* CalcMinor(matrix*, int, int);
+double CalcDeterminant(matrix*);
+int mtxlen1(matrix*);
+int mtxlen2(matrix*);
+double val(matrix*, int, int);
+void setval(matrix*, double, int, int);
+matrix* CreateMatrix(int, int);
+matrix* CreateOnesMatrix(int, int);
+matrix* mtxtrn(matrix*);
+matrix* mtxmul(matrix*, matrix*);
+matrix* mtxadd(matrix*, matrix*);
+matrix* mtxneg(matrix*);
+matrix* CalcAdj(matrix*);
+matrix* CalcInv(matrix*);
+
+matrix* ExtractColumn(matrix*, int);
+matrix* AugmentMatrix(matrix*, matrix*);
+
+matrix* ParseMatrix(char*);
+matrix* linspace(double, double, int);
+void Map(matrix*, double (*func)(double));
+void mtxprnt(matrix*);
+void mtxprntfile(matrix*, char*);
+
+typedef struct {
+    double *v;
+    int length;
+} vector;
+
+vector* CreateVector(int);
+void DestroyVector(vector*);
+//double valV(vector*, int);
+void setvalV(vector*, int, double);
+int len(vector*);
+void PrintVector(vector*);
+
+vector* addV(vector*, vector*);
+vector* subtractV(vector*, vector*);
+double dotV(vector*, vector*);
+vector* scalarmultV(double, vector*);
+int equalV(vector*, vector*);
+
+#define valV(VECTOR, INDEX) (VECTOR)->v[(INDEX)]
+
+#endif
---
+++ vector.c
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "matrix.h"
+
+/* Create a 1d vector of length n */
+vector* CreateVector(int n)
+{
+    vector *v;
+    v = (vector*) calloc(1, sizeof(vector));
+
+    v->v = (double*) calloc(n, sizeof(double));
+    v->length = n;
+
+    return v;
+}
+
+/* Free the memory allocated for the vector. */
+void DestroyVector(vector *v)
+{
+    free(v->v);
+    free(v);
+    return;
+}
+
+/* Print the vector to stdout */
+void PrintVector(vector *v)
+{
+    int i;
+
+    printf("[");
+    for(i=0; i<len(v)-1; i++) {
+        printf(" %g,", valV(v, i));
+    }
+    printf(" %g ]\n", valV(v, len(v)-1));
+}
+
+/* Return the i-th value */
+//double valV(vector *v, int i)
+//{
+//    if(i<0 || i>=v->length) {
+//        /* Index out of bounds */
+//        return 0;
+//    }
+//
+//    return v->v[i];
+//}
+/* Replaced by a macro for efficiency */
+
+/* Set the i-th value to "val" */
+void setvalV(vector *v, int i, double val)
+{
+    if(i<0 || i>=v->length) {
+        /* Index out of bounds */
+        return;
+    }
+
+    v->v[i] = val;
+    return;
+}
+
+/* Return the length of the vector */
+int len(vector* v)
+{
+    return v->length;
+}
+
+/* Add two vectors together */
+vector* addV(vector *a, vector *b)
+{
+    int i;
+    vector *c;
+    c = CreateVector(len(a));
+
+    for(i=0; i<len(a); i++) {
+        setvalV(c, i, valV(a, i) + valV(b, i));
+    }
+    return c;
+}
+
+/* Subtract vector b from vector a */
+vector* subtractV(vector *a, vector *b)
+{
+    int i;
+    vector *c;
+    c = CreateVector(len(a));
+
+    for(i=0; i<len(a); i++) {
+        setvalV(c, i, valV(a, i) - valV(b, i));
+    }
+    return c;
+}
+
+/* Return the dot product of two vectors */
+double dotV(vector *a, vector *b)
+{
+    int i;
+    double result = 0;
+
+    for(i=0; i<len(a); i++) {
+        result += valV(a, i) * valV(b, i);
+    }
+    return result;
+}
+
+/* Multiply a vector by a scalar */
+vector* scalarmultV(double k, vector *v)
+{
+    int i;
+    vector *c;
+
+    c = CreateVector(v->length);
+
+    for(i=0; i<len(v); i++) {
+        setvalV(c, i, k*valV(v, i));
+    }
+
+    return c;
+}
+
+int equalV(vector *a, vector *b)
+{
+    int i;
+    double tol = 1e-10;
+
+    if(a == b)
+        return 1;
+
+    if(len(a) != len(b)) {
+        return 0;
+    }
+
+    for(i=0; i<len(a); i++) {
+        if(fabs(valV(a, i) - valV(b, i)) >= tol)
+            return 0;
+    }
+
+    return 1;
+}
+
